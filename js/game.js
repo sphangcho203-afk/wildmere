2f58228d630a982e09d8558f89842738dc92ecb2
Songja Phangcho <sphangcho203@gmail.com>
2026-09-02 06:55:39 +0530

    Restore play loop and hook The Reed Step into the valley

--- js/game.js
+++ js/game.js
@@ -1,1 +1,13 @@
-PLACEHOLDER
+import * as THREE from 'three';
+import { Sky } from 'three/addons/objects/Sky.js';
+import {
+  WORLD, WATER_Y, SEED,
+  riverDist, heightAt,
+  makeTree, treeKindAt, addBerryBush,
+  makeHuman, makeRiverWater, currentPlace, makeStoneRing, makeMossSeat,
+  makeQuietWell, atQuietWell,
+  makeListeningPine, atListeningPine,
+  makeWindHollow, atWindHollow,
+  makeReedStep, atReedStep,
+  addDistantRidges, addGrassTufts, addValleyBirds, stepBirds
+} from './world.js';
