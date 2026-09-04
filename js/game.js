a861e0b69f9e867313410d792fc336277f3c6be5
Ike shanto <sphangcho203@gmail.com>
2026-09-04 06:57:00 +0530

    Restore Wildmere play loop with Shade Pool

--- js/game.js
+++ js/game.js
@@ -1,18 +1,1 @@
-import * as THREE from 'three';
-import { Sky } from 'three/addons/objects/Sky.js';
-import {
-  WORLD, WATER_Y, SEED,
-  riverDist, heightAt,
-  makeTree, treeKindAt, addBerryBush,
-  makeHuman, makeRiverWater, currentPlace, makeStoneRing, makeMossSeat,
-  makeQuietWell, atQuietWell,
-  makeListeningPine, atListeningPine,
-  makeWindHollow, atWindHollow,
-  makeReedStep, atReedStep,
-  makeLowCairn, atLowCairn,
-  makeShadePool, atShadePool,
-  addDistantRidges, addGrassTufts, addValleyBirds, stepBirds
-} from './world.js';
-import { atSlowBend, placeSlowBend } from './bend.js';
-import { loadNotes, saveNotes, noteForPlace, renderNotebook } from './notebook.js';
-import { makePassingRain, stepRain, rainWanted } from './weather.js';
+PLACEHOLDER_USE_FILE
