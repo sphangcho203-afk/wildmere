7b904352f98cb66cafa29a8a70079f40a36b3d7e
Ike shanto <sphangcho203@gmail.com>
2026-09-15 06:57:07 +0530

    Compass names The Rush Nest first

--- js/places.js
+++ js/places.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { WATER_Y, heightAt, fbm, riverDist, atStoneTerrace, TERRACE_X, TERRACE_Z } from './world.js';
 import { atDaisyRing } from './daisy.js';
+import { atRushNest } from './rush.js';
 
 function hash2(ix, iy){
   const SEED = 2041;
@@ -248,6 +249,7 @@ export function makeRiverWater(waterMat){
   return group;
 }
 export function currentPlace(x, z){
+  if (atRushNest(x, z)) return 'The Rush Nest';
   if (atDaisyRing(x, z)) return 'The Daisy Ring';
   if (atStoneTerrace(x, z)) return 'The Stone Terrace';
   if (atQuietWell(x, z)) return 'The Quiet Well';
